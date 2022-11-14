74c1ea1e741db6ea0d92ec070a4c711ff1fd571b
dotfury <cm2jr4@gmail.com>
2022-11-14 18:11:26 +0900

    implement stack - 2022/11/14

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,46 @@
+type Node<T> = {
+  value: T;
+  prev?: Node<T>;
+};
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head: Node<T> | undefined;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  push(item: T): void {
+    this.length++;
+    const newItem = { value: item } as Node<T>;
+
+    if (!this.head) {
+      this.head = newItem;
+      return;
     }
 
-    push(item: T): void {
+    newItem.prev = this.head;
+    this.head = newItem;
+  }
 
-}
-    pop(): T | undefined {
+  pop(): T | undefined {
+    this.length = Math.max(0, this.length - 1);
+    if (this.length === 0) {
+      const head = this.head;
+      this.head = undefined;
 
-}
-    peek(): T | undefined {
+      return head?.value;
+    }
 
-}
+    const head = this.head as Node<T>;
+    this.head = head.prev;
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
