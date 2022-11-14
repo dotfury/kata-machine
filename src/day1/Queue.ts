9c038eec15a0efd8e268c6deebefef4da720e4d5
dotfury <cm2jr4@gmail.com>
2022-11-14 11:39:31 +0900

    implement queue - 2022/11/11

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,48 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    this.length++;
+    const newItem = { value: item } as Node<T>;
+    if (!this.tail) {
+      this.head = this.tail = newItem;
+      return;
     }
 
-    enqueue(item: T): void {
+    this.tail.next = newItem;
+    this.tail = newItem;
+  }
 
-}
-    deque(): T | undefined {
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
 
-}
-    peek(): T | undefined {
+    this.length--;
+    const head = this.head;
+    this.head = head.next;
+    head.next = undefined;
 
-}
+    if (this.length === 0) {
+      this.tail = undefined;
+    }
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
