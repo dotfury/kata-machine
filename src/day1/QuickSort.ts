a92d7be1ec6809566d3c517cc0f3ee7bc2c7c55a
dotfury <cm2jr4@gmail.com>
2022-11-29 12:32:31 +0900

    understanding quicksort - 2022/11/28

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -3,8 +3,11 @@ function qs(arr: number[], lo: number, hi: number): void {
     return;
   }
 
-  const pivotIndex = partition(arr, lo, hi);
+  console.log("LO: ", lo);
+  console.log("HI: ", hi);
 
+  const pivotIndex = partition(arr, lo, hi);
+  console.log("PIVOT INDEX: ", pivotIndex);
   qs(arr, lo, pivotIndex - 1);
   qs(arr, pivotIndex + 1, hi);
 }
@@ -12,24 +15,33 @@ function qs(arr: number[], lo: number, hi: number): void {
 function partition(arr: number[], lo: number, hi: number): number {
   const pivot = arr[hi];
   let index = lo - 1;
-
+  console.log("current index: ", index);
   for (let i = lo; i < hi; ++i) {
     if (arr[i] <= pivot) {
+      console.log(`arr[${i}]: ${arr[i]} smaller than pivot: ${pivot}`);
       index++;
+      console.log("i: ", i);
+      console.log("new index: ", index);
       const temp = arr[i];
       arr[i] = arr[index];
       arr[index] = temp;
+
+      console.log("new array: ", arr);
     }
   }
 
   index++;
 
+  console.log(`swap ${arr[index]} and ${pivot}`);
   arr[hi] = arr[index];
   arr[index] = pivot;
+
+  console.log("ARRAY: ", arr);
 
   return index;
 }
 
 export default function quick_sort(arr: number[]): void {
+  console.log("START: ", arr);
   qs(arr, 0, arr.length - 1);
 }
