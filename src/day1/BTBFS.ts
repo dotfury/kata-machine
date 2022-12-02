4a220d1ac0bdff18d997b048757aa87daa4a18a2
dotfury <cm2jr4@gmail.com>
2022-12-02 12:52:45 +0900

    breadth first search - 2022/11/02

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,20 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const q: (BinaryNode<number> | null)[] = [head];
 
+  while (q.length) {
+    const current = q.shift() as BinaryNode<number> | undefined | null;
+
+    if (!current) {
+      continue;
+    }
+
+    if (current.value === needle) {
+      return true;
+    }
+
+    q.push(current.left);
+    q.push(current.right);
+  }
+
+  return false;
 }
