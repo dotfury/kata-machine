94516413e54e64b383d828ba283b8a4e1a56308f
dotfury <cm2jr4@gmail.com>
2022-12-13 11:46:07 +0900

    deapth first search - 2022/12/13

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,21 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(current: BinaryNode<number> | null, needle: number): boolean {
+  if (!current) {
+    return false;
+  }
 
+  if (current.value === needle) {
+    return true;
+  }
+
+  // search for larger value
+  if (current.value < needle) {
+    return search(current.right, needle);
+  }
+
+  // search for smaller value
+  return search(current.left, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+  return search(head, needle);
 }
